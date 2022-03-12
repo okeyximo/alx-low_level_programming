f8f27891a36c046f55480244e4c3289b03b02ff7
okeyximo <xcellnximo57@gmail.com>
2022-03-12 09:33:33 +0000

    Returns numbers from 0 - 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,17 +3,18 @@
 /**
  * main - Prints all single digit numbers of base 10 starting from zero
  *
- * Return - 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	int n = 0;
+
 	while (n <= 9)
 	{
 		printf("%d", n);
 		n++;
 	}
 	printf("\n");
-	return(0);
+	return (0);
 }
 
