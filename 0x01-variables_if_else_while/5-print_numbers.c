36743837abc82376152691b80a643d99ee690489
okeyximo <xcellnximo57@gmail.com>
2022-03-12 02:57:30 +0000

    Print all numbers 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single base ten numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+	return 0;
+}
