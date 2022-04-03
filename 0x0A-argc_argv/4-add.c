d0a844a84e06870acd3661900182426275d567a3
okeyximo <xcellnximo57@gmail.com>
2022-04-03 15:49:12 +0000

    commit 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,16 +1,32 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - prints the name of the program
+ * main - prints the results of positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
 
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc , char **argv)
 {
-	printf("%s\n", *argv);
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
