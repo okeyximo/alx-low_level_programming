b3d6a6b59e5c046e0a1226c0a100df13be811692
okeyximo <xcellnximo57@gmail.com>
2022-04-05 14:55:33 +0000

    fixed error 2nd commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,10 +13,10 @@ char *_strdup(char *str)
 	char *pstr;
 	int i = 0, len = 0;
 
-	while (str[len])
-		len++;
 	if (str == NULL)
 		return (NULL);
+	while (str[len])
+		len++;
 	pstr = (char *)malloc((sizeof(char)) * (len + 1));
 	if (pstr == NULL)
 		return (NULL);
