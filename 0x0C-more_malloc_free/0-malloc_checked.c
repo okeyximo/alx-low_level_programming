467d1ab4dc826e436bdb2cab576001f6c77fb878
okeyximo <xcellnximo57@gmail.com>
2022-04-07 19:00:47 +0000

    fixed 1st commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,11 +5,13 @@
  * malloc_checked - Allocates memory using malloc
  *
  * @b: passed unsigned int variable
+ * Return: the pointer or error message 98
  */
 
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *pb;
+
 	pb = malloc(b);
 	if (pb == NULL)
 		exit(98);
