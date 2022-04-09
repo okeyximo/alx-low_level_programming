f04dc2666e025f110200b81eecb9543b9e8f09fd
okeyximo <xcellnximo57@gmail.com>
2022-04-09 04:07:29 +0000

    fixed 3-array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (max - min + 2));
+	arr = malloc(sizeof(int) * (max - min + 1));
 	if (!arr)
 		return (NULL);
 
