75a21fd1966b23111138f5d4892076c118a50cdb
okeyximo <xcellnximo57@gmail.com>
2022-04-12 21:16:19 +0000

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - describes the common features of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
