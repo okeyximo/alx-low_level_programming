5125668d9406409c787492f33b5b0dd9f4a4a266
okeyximo <xcellnximo57@gmail.com>
2022-04-13 09:01:20 +0000

    fixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 	if (!d->name)
-		d->name = "(nill)";
+		d->name = "(nil)";
 	if (!d->owner)
-		d->owner = "(nill)";
+		d->owner = "(nil)";
 
 	if (d)
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
