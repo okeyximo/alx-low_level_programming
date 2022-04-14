91b3ff02713b1ddc33c379fa9a78ee69f08cfb6f
okeyximo <xcellnximo57@gmail.com>
2022-04-14 05:01:21 +0000

    fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,10 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
-					return (i);
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
