58702dbd2dc34c5acccfd99c4d75e02688396484
okeyximo <xcellnximo57@gmail.com>
2022-04-15 23:45:19 +0000

    1st commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of arguments to be passed
+ * Return: 0 or SUM
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,8 @@
+## Some C task on how to use variadic functions 
+1. Write a function that returns the sum of all its parameters.
+2. Write a function that prints numbers, followed by a new line.
+3. Write a function that prints anything.
+
+
+
+
