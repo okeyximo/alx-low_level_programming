82de4cb4c4bb95a8c2368ef9e33733f2911861ad
okeyximo <xcellnximo57@gmail.com>
2022-04-15 21:34:11 +0000

    omo!!

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,27 +1,33 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - selects the correct function to perform by the user
- * @s: the operator passed as argument to the program
- * Return: NULL or the expected operators
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
  */
-
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
 
 	while (ops[i].op)
 	{
-		if (strcpy(ops[i].op, s) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
+		i++;
 	}
+
 	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,15 +1,17 @@
 #include "3-calc.h"
+
 /**
- * main - calculator
- * @argc: argument count
- * @argv: argument vector
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int arg1, arg2, result;
+	char o;
 	int (*func)(int, int);
-
 
 	if (argc != 4)
 	{
@@ -17,17 +19,28 @@ int main(int argc,char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
+
 	if (!func)
 	{
-		printf("Error/n");
-		return (99);
+		printf("Error\n");
+		exit(99);
 	}
-	printf("%d\n", func(num1, num2));
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
 
 	return (0);
-
 }
