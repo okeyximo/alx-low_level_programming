51fc60e5ff267caa2e11f8985482fe0e24ac1630
okeyximo <xcellnximo57@gmail.com>
2022-04-22 22:20:27 +0000

    fixed betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,6 +4,7 @@
  * add_node_end - adds a new node at the end of a list_t list
  * @head: double pointer to the head pointer
  * @str: str passed
+ * Return: address of the new element or NULL
  */
 
 list_t *add_node_end(list_t **head, const char *str)
