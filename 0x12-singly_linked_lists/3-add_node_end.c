8fbee7db96018da32ef94016318c769caa4a3485
okeyximo <xcellnximo57@gmail.com>
2022-04-22 22:12:18 +0000

    fixed number 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: double pointer to the head pointer
+ * @str: str passed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp = *head;
+	unsigned int len = 0;
+	list_t *new;
+
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->len = len;
+	new->str = strdup(str);
+	new->next = NULL;
+
+	if (!(*head))
+	{
+		(*head) = new;
+		return (new);
+	}
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+
+	return (new);
+}
