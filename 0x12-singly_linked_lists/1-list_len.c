b6b85e04785fcf5649e6d36a2f5b8efdf925b0f8
okeyximo <xcellnximo57@gmail.com>
2022-04-22 13:05:41 +0000

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements linked list_t
+ * @h: node pointer to the list_t
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	return(len);
+}
