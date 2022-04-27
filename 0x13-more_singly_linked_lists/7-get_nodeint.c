ea31fa08d47ead8a2e2026a39c4ce74f249ed3b3
okeyximo <xcellnximo57@gmail.com>
2022-04-27 23:49:34 +0000

    fixed again

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int n = 0;
 
-	if (temp && n < index)
+	while (temp && n < index)
 	{
 		temp = temp->next;
 		n++;
