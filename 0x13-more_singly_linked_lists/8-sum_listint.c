9aaca8fd720489768c0ce461a2bbad8dccce824b
okeyximo <xcellnximo57@gmail.com>
2022-04-28 00:11:49 +0000

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums all the data of list
+ * @head: the head pointer to the head node
+ * Return: sum or 0;
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	if (!head)
+		return (0);
+	while(temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+
+}
